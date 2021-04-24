0b237262825ad775729873f3311b56a2be8d166b
Sam Carbine <scarbine@me.com>
2021-04-23 20:45:44 -0500

    First Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,33 @@
+
+const foodConsumed = [];
+const currentMeal = {
+    id:"",
+    foodName:"",
+    foodQuanity:0,
+    timeConsumed:"",
+    dateConsumed:"",
+}
+let totalDailyCalories=0;
+let totalDailyFat=0;
+let totalDailyProtien=0;
+let totalDailyCarbs=0;
+
+const total_calories_daily_div = document.getElementById("total_calories_daily")
+const total_carbs_daily_div = document.getElementById("total_carbs_daily")
+const total_fats_daily_div = document.getElementById("total_fats_daily")
+const total_calories_daily_div = document.getElementById("total_protein_daily")
+
+// const total_calories_daily_div = document.getElementById("total_calories_daily")
+// const total_carbs_daily_div = document.getElementById("total_carbs_daily")
+// const total_fats_daily_div = document.getElementById("total_fats_daily")
+// const total_calories_daily_div = document.getElementById("total_protein_daily")
+
+
+
+
+
+
+// this function adds a new food to the food Consummed array once the user has subbited valid data in the input fields 
+const addFood = () => {
+
+}
