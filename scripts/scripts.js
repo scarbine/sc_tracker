d5ad286acdcd3a8eb0613eade40af979bc588039
scarbine <80789997+scarbine@users.noreply.github.com>
2021-04-24 14:20:20 -0500

    quick backup

---
+++ scripts/scripts.js
@@ -0,0 +1,125 @@
+
+
+// cashe the DOM
+
+const total_calories_daily_div = document.getElementById("total_calories_daily");
+const total_carbs_daily_div = document.getElementById("total_carbs_daily");
+const total_fats_daily_div = document.getElementById("total_fats_daily");
+const total_protein_daily_div = document.getElementById("total_protein_daily");
+
+const barcode_div = document.getElementById('new_food_barcode');
+const foodName_div = document.getElementById('new_food_name');
+const calories_div = document.getElementById('new_food_calories');
+const fat_div =  document.getElementById('new_food_fat');
+const protein_div = document.getElementById('new_food_protein');
+const carbs_div = document.getElementById('new_food_carbs');
+   
+
+// const total_calories_daily_div = document.getElementById("total_calories_daily")
+// const total_carbs_daily_div = document.getElementById("total_carbs_daily")
+// const total_fats_daily_div = document.getElementById("total_fats_daily")
+// const total_calories_daily_div = document.getElementById("total_protein_daily")
+
+
+const foodConsumed = [];
+const currentMeal = {
+    barcode:"",
+    foodName:"",
+    foodQuanity:0,
+    timeConsumed:"",
+    dateConsumed:"",
+}
+let totalDailyCalories=0;
+let totalDailyFat=0;
+let totalDailyProtien=0;
+let totalDailyCarbs=0;
+
+let foodLogCounterid = '000000000000'
+
+let foodLog = [
+    {
+        id: "000000000000",
+        currentUserid : 00000,
+        foodName: '',
+        foodIntakeTime: '',
+        foodIntakeDate: '',
+
+    }
+]
+// foods added to this come from all useser serving a collective database that can be accessed by any useser when adding food
+let foodList = [];
+
+// this section holds user data
+let currentActiveUser= 00000
+let currentUser = [
+    
+    { 
+        id: '00001',
+        name: "Sam"
+    },
+
+
+    {
+        id: '00002',  
+        name: "Holden"
+        
+    },
+
+
+    {
+        id: '00003',  
+        name: "Bella"
+    }
+
+]
+
+//get current date
+let currentDate= () =>{
+    let n = new Date();
+    let y = n.getFullYear();
+    let m = n.getMonth() + 1;
+    let d = n.getDate();
+    document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+    console.log( m + "/" + d + "/" + y)
+
+}
+currentDate();
+
+//get currentUSer at login - currenlty just assiging an id to the function call staticly so variables can be tested in the DOM
+
+let getCurrentUser = (login) => {
+    for (let i = 0; i < currentUser.length; i++){
+        
+        if (currentUser[i].id === login){
+            currentActiveUser = currentUser[i].name;
+            console.log(currentActiveUser)
+            document.getElementById("nav_header").innerHTML = `${currentActiveUser}'s Food Intake`;
+            return
+        }
+        else{
+        const userNotFound = 'User could not be found';
+        document.getElementById("nav_header").innerHTML = `${userNotFound}`;
+        }
+    }
+}
+
+getCurrentUser('00001');
+
+// this function adds a new food to the foodList array once the user has hit the submit button
+const addNEwFood = () => {
+   let food = {
+        barcode: barcode_div.value,
+        foodName: foodName_div.value,
+        calories: calories_div.value,
+        fat: fat_div.value,
+        protein: protein_div.value,
+        carbs: carbs_div.value,
+   }
+   console.log(food)    
+   foodList.push(food)
+   console.log(foodList)
+
+   localStorage.setItem('Food List', JSON.stringify(foodList));
+}
+
+document.getElementById('add_new_food_button').addEventListener('click', addNEwFood)
